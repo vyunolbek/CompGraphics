b125144b58a7c243f296220237ce62d3a5e53ed1
Данила Вьюнов <cliffordik14@gmail.com>
2023-03-13 21:04:27 +0300

    Кроме мат. морфологии

--- Графика/Form1.Designer.cs
+++ Графика/Form1.Designer.cs
@@ -34,14 +34,20 @@
             фильтрыToolStripMenuItem = new ToolStripMenuItem();
             точечныеToolStripMenuItem = new ToolStripMenuItem();
             инверсияToolStripMenuItem = new ToolStripMenuItem();
+            оттенкиСерогоToolStripMenuItem = new ToolStripMenuItem();
+            сепияToolStripMenuItem = new ToolStripMenuItem();
+            изменитьЯркостьToolStripMenuItem = new ToolStripMenuItem();
             матричныеToolStripMenuItem = new ToolStripMenuItem();
+            размытиеToolStripMenuItem = new ToolStripMenuItem();
+            фильтрГауссаToolStripMenuItem = new ToolStripMenuItem();
+            резкостьToolStripMenuItem = new ToolStripMenuItem();
+            соебльToolStripMenuItem = new ToolStripMenuItem();
             pictureBox1 = new PictureBox();
             progressBar1 = new ProgressBar();
             button1 = new Button();
             backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
-            размытиеToolStripMenuItem = new ToolStripMenuItem();
             menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -78,24 +84,73 @@
             // 
             // точечныеToolStripMenuItem
             // 
-            точечныеToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { инверсияToolStripMenuItem });
+            точечныеToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { инверсияToolStripMenuItem, оттенкиСерогоToolStripMenuItem, сепияToolStripMenuItem, изменитьЯркостьToolStripMenuItem });
             точечныеToolStripMenuItem.Name = "точечныеToolStripMenuItem";
-            точечныеToolStripMenuItem.Size = new Size(180, 22);
+            точечныеToolStripMenuItem.Size = new Size(139, 22);
             точечныеToolStripMenuItem.Text = "Точечные";
             // 
             // инверсияToolStripMenuItem
             // 
             инверсияToolStripMenuItem.Name = "инверсияToolStripMenuItem";
-            инверсияToolStripMenuItem.Size = new Size(180, 22);
+            инверсияToolStripMenuItem.Size = new Size(174, 22);
             инверсияToolStripMenuItem.Text = "Инверсия";
             инверсияToolStripMenuItem.Click += инверсияToolStripMenuItem_Click;
             // 
+            // оттенкиСерогоToolStripMenuItem
+            // 
+            оттенкиСерогоToolStripMenuItem.Name = "оттенкиСерогоToolStripMenuItem";
+            оттенкиСерогоToolStripMenuItem.Size = new Size(174, 22);
+            оттенкиСерогоToolStripMenuItem.Text = "Оттенки серого";
+            оттенкиСерогоToolStripMenuItem.Click += оттенкиСерогоToolStripMenuItem_Click;
+            // 
+            // сепияToolStripMenuItem
+            // 
+            сепияToolStripMenuItem.Name = "сепияToolStripMenuItem";
+            сепияToolStripMenuItem.Size = new Size(174, 22);
+            сепияToolStripMenuItem.Text = "Сепия";
+            сепияToolStripMenuItem.Click += сепияToolStripMenuItem_Click;
+            // 
+            // изменитьЯркостьToolStripMenuItem
+            // 
+            изменитьЯркостьToolStripMenuItem.Name = "изменитьЯркостьToolStripMenuItem";
+            изменитьЯркостьToolStripMenuItem.Size = new Size(174, 22);
+            изменитьЯркостьToolStripMenuItem.Text = "Изменить яркость";
+            изменитьЯркостьToolStripMenuItem.Click += изменитьЯркостьToolStripMenuItem_Click;
+            // 
             // матричныеToolStripMenuItem
             // 
-            матричныеToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { размытиеToolStripMenuItem });
+            матричныеToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { размытиеToolStripMenuItem, фильтрГауссаToolStripMenuItem, резкостьToolStripMenuItem, соебльToolStripMenuItem });
             матричныеToolStripMenuItem.Name = "матричныеToolStripMenuItem";
-            матричныеToolStripMenuItem.Size = new Size(180, 22);
+            матричныеToolStripMenuItem.Size = new Size(139, 22);
             матричныеToolStripMenuItem.Text = "Матричные";
+            // 
+            // размытиеToolStripMenuItem
+            // 
+            размытиеToolStripMenuItem.Name = "размытиеToolStripMenuItem";
+            размытиеToolStripMenuItem.Size = new Size(154, 22);
+            размытиеToolStripMenuItem.Text = "Размытие";
+            размытиеToolStripMenuItem.Click += размытиеToolStripMenuItem_Click;
+            // 
+            // фильтрГауссаToolStripMenuItem
+            // 
+            фильтрГауссаToolStripMenuItem.Name = "фильтрГауссаToolStripMenuItem";
+            фильтрГауссаToolStripMenuItem.Size = new Size(154, 22);
+            фильтрГауссаToolStripMenuItem.Text = "Фильтр Гаусса";
+            фильтрГауссаToolStripMenuItem.Click += фильтрГауссаToolStripMenuItem_Click;
+            // 
+            // резкостьToolStripMenuItem
+            // 
+            резкостьToolStripMenuItem.Name = "резкостьToolStripMenuItem";
+            резкостьToolStripMenuItem.Size = new Size(154, 22);
+            резкостьToolStripMenuItem.Text = "Резкость";
+            резкостьToolStripMenuItem.Click += резкостьToolStripMenuItem_Click;
+            // 
+            // соебльToolStripMenuItem
+            // 
+            соебльToolStripMenuItem.Name = "соебльToolStripMenuItem";
+            соебльToolStripMenuItem.Size = new Size(154, 22);
+            соебльToolStripMenuItem.Text = "Собель";
+            соебльToolStripMenuItem.Click += собельToolStripMenuItem_Click;
             // 
             // pictureBox1
             // 
@@ -118,7 +173,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 27);
             button1.TabIndex = 3;
-            button1.Text = "button1";
+            button1.Text = "Отмена";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -129,13 +184,6 @@
             backgroundWorker1.DoWork += backgroundWorker1_DoWork;
             backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
             backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
-            // 
-            // размытиеToolStripMenuItem
-            // 
-            размытиеToolStripMenuItem.Name = "размытиеToolStripMenuItem";
-            размытиеToolStripMenuItem.Size = new Size(180, 22);
-            размытиеToolStripMenuItem.Text = "Размытие";
-            размытиеToolStripMenuItem.Click += размытиеToolStripMenuItem_Click;
             // 
             // Form1
             // 
@@ -173,5 +221,11 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private System.ComponentModel.BackgroundWorker backgroundWorker3;
         private ToolStripMenuItem размытиеToolStripMenuItem;
+        private ToolStripMenuItem фильтрГауссаToolStripMenuItem;
+        private ToolStripMenuItem оттенкиСерогоToolStripMenuItem;
+        private ToolStripMenuItem сепияToolStripMenuItem;
+        private ToolStripMenuItem изменитьЯркостьToolStripMenuItem;
+        private ToolStripMenuItem резкостьToolStripMenuItem;
+        private ToolStripMenuItem соебльToolStripMenuItem;
     }
 }
--- Графика/Form1.cs
+++ Графика/Form1.cs
@@ -9,7 +9,7 @@ namespace Графика
         {
             protected abstract Color calculateNewPixel(Bitmap sourceImage, int x, int y);
 
-            public int Clamp(int min, int max, int value)
+            public int Clamp(int value, int min, int max)
             {
                 if (value < min) return min;
                 if (value > max) return max;
@@ -41,6 +41,38 @@ namespace Графика
                 Color sourceColor = sourceImage.GetPixel(x, y);
                 Color resultColor = Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
                 return resultColor;
+            }
+        }
+
+        class GrayScaleFilter : Filters
+        {
+            protected override Color calculateNewPixel(Bitmap sourceImage, int x, int y)
+            {
+                Color sourceIntencity = sourceImage.GetPixel(x, y);
+                int intencity = (int)((sourceIntencity.R * 0.36) + (sourceIntencity.G * 0.51) + (sourceIntencity.B * 0.11));
+                Color resultIntencity = Color.FromArgb(intencity, intencity, intencity);
+                return resultIntencity;
+            }
+        }
+
+        class SepiaFilter : Filters
+        {
+            protected override Color calculateNewPixel(Bitmap sourceImage, int x, int y)
+            {
+                Color sourceIntencity = sourceImage.GetPixel(x, y);
+                int intencity = (int)((sourceIntencity.R * 0.36) + (sourceIntencity.G * 0.51) + (sourceIntencity.B * 0.11));
+                Color result = Color.FromArgb(Clamp(intencity + 2 * 20, 0, 255), Clamp(intencity + (int)0.5 * 20, 0, 255), Clamp((intencity - 1 * 20), 0, 255));
+                return result;
+            }
+        }
+
+        class Brightness : Filters
+        {
+            protected override Color calculateNewPixel(Bitmap sourceImage, int x, int y)
+            {
+                Color sourceIntencity = sourceImage.GetPixel(x, y);
+                Color result = Color.FromArgb(Clamp(sourceIntencity.R + 30, 0, 225), Clamp(sourceIntencity.G + 30, 0, 255), Clamp(sourceIntencity.B + 30, 0, 255));
+                return result;
             }
         }
 
@@ -78,6 +110,17 @@ namespace Графика
             }
         }
 
+        class Sharpness : MatrixFilter
+        {
+            public Sharpness()
+            {
+                kernel = new float[,] {  {0, -1,  0 },
+                                         {-1, 5, -1 },
+                                         {0, -1,  0 } };
+
+            }
+        }
+
         class BlurFilter : MatrixFilter
         {
             public BlurFilter()
@@ -91,7 +134,106 @@ namespace Графика
             }
         }
 
+        class GaussianFilter : MatrixFilter
+        {
+            public GaussianFilter()
+            {
+                createGaussianKernel(3, 10);
+            }
 
+            public void createGaussianKernel(int radius, float sigma)
+            {
+                int size = 2 * radius + 1;
+                kernel = new float[size, size];
+                float norm = 0;
+                for (int i = -radius; i <= radius; i++)
+                    for (int j = -radius; j <= radius; j++)
+                    {
+                        kernel[i + radius, j + radius] = (float)(Math.Exp(-(i * i + j * j) / (sigma * sigma)));
+                        norm += kernel[i + radius, j + radius];
+                    }
+
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        kernel[i, j] /= norm;
+            }
+        }
+
+        class DoubleMatrix : Filters
+        {
+            protected float[,] kernel1 = null;
+            protected float[,] kernel2 = null;
+
+            protected DoubleMatrix() { }
+
+            public DoubleMatrix(float[,] kernel1, float[,] kernel2)
+            {
+                this.kernel1 = kernel1;
+                this.kernel2 = kernel2;
+            }
+
+            protected override Color calculateNewPixel(Bitmap sourceImage, int x, int y)
+            {
+                int radiusX = kernel1.GetLength(0) / 2;
+                int radiusY = kernel1.GetLength(1) / 2;
+
+                float resultR1 = 0;
+                float resultG1 = 0;
+                float resultB1 = 0;
+
+                for (int i = -radiusY; i <= radiusY; i++)
+                {
+                    for (int k = -radiusX; k <= radiusX; k++)
+                    {
+                        int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+                        int idY = Clamp(y + i, 0, sourceImage.Height - 1);
+
+                        Color neighbor = sourceImage.GetPixel(idX, idY);
+
+                        resultR1 += neighbor.R * kernel1[k + radiusX, i + radiusY];
+                        resultG1 += neighbor.G * kernel1[k + radiusX, i + radiusY];
+                        resultB1 += neighbor.B * kernel1[k + radiusX, i + radiusY];
+                    }
+                }
+
+                int radiusX2 = kernel2.GetLength(0) / 2;
+                int radiusY2 = kernel2.GetLength(1) / 2;
+
+                float resultR2 = 0;
+                float resultG2 = 0;
+                float resultB2 = 0;
+
+                for (int i = -radiusY2; i <= radiusY2; i++)
+                {
+                    for (int k = -radiusX2; k <= radiusX2; k++)
+                    {
+                        int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+                        int idY = Clamp(y + i, 0, sourceImage.Height - 1);
+
+                        Color neighbor = sourceImage.GetPixel(idX, idY);
+
+                        resultR2 += neighbor.R * kernel2[k + radiusX2, i + radiusY2];
+                        resultG2 += neighbor.G * kernel2[k + radiusX2, i + radiusY2];
+                        resultB2 += neighbor.B * kernel2[k + radiusX2, i + radiusY2];
+                    }
+                }
+
+                return Color.FromArgb(
+         Clamp((int)Math.Sqrt((resultR1 * resultR1 + resultR2 * resultR2)), 0, 255),
+         Clamp((int)Math.Sqrt((resultG1 * resultG1 + resultG2 * resultG2)), 0, 255),
+         Clamp((int)Math.Sqrt((resultB1 * resultB1 + resultB2 * resultB2)), 0, 255));
+            }
+
+        }
+
+        class Sobel : DoubleMatrix
+        {
+            public Sobel()
+            {
+                kernel1 = new float[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+                kernel2 = new float[,] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+            }
+        }
 
         Bitmap image;
 
@@ -154,10 +296,43 @@ namespace Графика
         private void размытиеToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Filters filter = new BlurFilter();
-            Bitmap resultImage = filter.processImage(image, backgroundWorker1);
             backgroundWorker1.RunWorkerAsync(filter);
-            pictureBox1.Image = resultImage;
-            pictureBox1.Refresh();
+        }
+
+        private void фильтрГауссаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GaussianFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void оттенкиСерогоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GrayScaleFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void сепияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new SepiaFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void изменитьЯркостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Brightness();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void резкостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Sharpness();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void собельToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Sobel();
+            backgroundWorker1.RunWorkerAsync(filter);
         }
     }
 }
